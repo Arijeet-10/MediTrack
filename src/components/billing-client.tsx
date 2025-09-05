4b5f5e0db5907c3f53bd9981fb93dc929fd9f9f3
Sayan Kr. Rakshit <sayan12842@gmail.com>
2025-09-05 09:09:17 +0000

    in the option create invoice, change it into generate bill

--- src/components/billing-client.tsx
+++ src/components/billing-client.tsx
@@ -59,7 +59,7 @@ export function BillingClient({
           <Button size="sm" className="h-8 gap-1">
             <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Create Invoice
+              Generate Bill
             </span>
           </Button>
         </div>
