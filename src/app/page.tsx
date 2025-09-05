88c3429d9ec748eed3250485a04e765b3bd82050
Sayan Kr. Rakshit <sayan12842@gmail.com>
2025-09-05 07:25:49 +0000

    there will no option like sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,12 +86,6 @@ export default function LoginPage() {
             </CardContent>
           </Card>
         </TabsContent>
-         <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
-          </div>
       </Tabs>
     </div>
   );
